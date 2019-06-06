91b9eb235b5f6289d35bcb0ee9e73733204ce352
Yoga <googagagaga@gmail.com>
2019-06-06 10:45:39 +0800

    GUI.py update

--- GUI.py
+++ GUI.py
@@ -1,41 +1,63 @@
-from tkinter import Label, Tk, Frame, Button, Entry, StringVar, Text, END
+from tkinter import Label, Tk, Frame, Button, Entry, StringVar, Text, messagebox, END
 from tkinter.ttk import Combobox
 from tkinter.scrolledtext import ScrolledText
-from pandastable import Table, TableModel
+from pandastable import Table
+from pandas import Timestamp, DataFrame
+from pandas._libs.tslibs.nattype import NaTType
+from database import stockDB, config
 
-class ButtonMode(Frame):
+class Mode:
+    @classmethod
+    def set_read_method(cls, func):
+        cls.read = func
+
+class ButtonMode(Frame, Mode):
     def __init__(self, parent):
         Frame.__init__(self, parent, bg=LINECOLOR)
+        self.q = [ StringVar(), StringVar(), StringVar(), StringVar() ]
         for i, t in enumerate(['商品', '開始日期', '結束日期', '頻率']):
             Label(self, text=t).grid(row=0, column=i, **PAD)
-        self.q = [ StringVar(), StringVar(), StringVar(), StringVar() ]
-        Combobox(self, values=['A', 'B', 'C', 'D'],
+        Combobox(self, values=['FITX'],
                     textvariable = self.q[0], width=10 ).grid(row=1, column=0)
         Entry(self, textvariable = self.q[1], width=10 ).grid(row=1, column=1)
         Entry(self, textvariable = self.q[2], width=10 ).grid(row=1, column=2)
-        Combobox(self, values = ['minute', 'day', 'week', 'month', 'year'], 
+        Combobox(self, values = ['minute', 'day'], 
                     textvariable = self.q[3], width=10 ).grid(row=1, column=3)
 
     @property
     def query(self):
-        """translate this into MySQL"""
-        return ( *map(lambda x: x.get(), self.q), )
+        try:
+            start = Timestamp(self.q[1].get())
+            end = Timestamp(self.q[2].get())
+            assert not isinstance(start, NaTType) and not isinstance(end, NaTType)
+        except Exception as e:
+            messagebox.showinfo('Wrong Format', 'Fail to convert into Timestamp')
+        else:
+            sDate = start.strftime('%Y-%m-%d')
+            eDate = end.strftime('%Y-%m-%d')
+            sTime = start.strftime('%H:%M:%S')
+            eTime = end.strftime('%H:%M:%S')
+            if self.q[3].get() == 'day':
+                return self.read(sDate, eDate)
+            else:
+                return self.read(sDate, eDate, sTime, eTime)
 
 
-class MySQLMode(ScrolledText):
+class SQLMode(ScrolledText, Mode):
     def __init__(self, parent):
         ScrolledText.__init__(self, parent, height=8, width=44)
 
     @property
     def query(self):
-        return self.get(0.0, END)
-
+        return self.read( self.get(0.0, END) )
 
 class ModeSelect(Combobox):
-    def __init__(self, parent, values, **kargs):
+    def __init__(self, parent, option, **kargs):
+        values = [ m.__name__.replace('Mode', '') for m in option ]
         self.state  = StringVar()
+        self.option = dict( zip(values, option) )
         Combobox.__init__(self, parent, values=values, textvariable = self.state, **kargs)
-        self.mode = ButtonMode( keyword_frame )
+        self.mode = self.option[values[0]]( keyword_frame )
         self.mode.pack()
         self.current(0)
         self.bind("<<ComboboxSelected>>", self.select)
@@ -45,65 +67,53 @@ class ModeSelect(Combobox):
         for widget in keyword_frame.winfo_children():
             widget.destroy()
         # create new one
-        if self.state.get() == 'Button':
-            self.mode = ButtonMode( keyword_frame )
-        elif self.state.get() == 'MySQL':
-            self.mode = MySQLMode( keyword_frame )
+        self.mode = self.option[self.state.get()](keyword_frame)
         self.mode.pack()
 
-def update():
-    data.model.df = database_query( mode_select.mode.query )
-    data.redraw()
-
-def database_query(query_string):
-    """
-    Write your code here,
-    Input
-    -----
-    query_string, str/tuple, depends on select mode
-
-    Return
-    ------
-    pandas.DataFrame
-    """
-    global i 
-    i += 1
-    print(query_string)
-    return df.head(i)
+    def link(self, table):
+        def update():
+            table.model.df = self.mode.query.head(300)
+            table.redraw()
+        return update
 
 LINECOLOR = 'lightgray'
 BOXCOLOR = 'palegreen'
 PAD = {'padx': 1, 'pady': 1, 'sticky': 'nsew' }
-df = TableModel.getSampleData()
 
-window = Tk()
-window.title("Arlen's money tree")
+if __name__ == "__main__":
+    mydb = stockDB(**config)
 
-manipulate_frame = Frame(window, bg=LINECOLOR) 
-data_frame = Frame(window, bg= LINECOLOR)
-manipulate_frame.pack(padx=1, pady=1)
-data_frame.pack(padx=1, pady=1)
-#                                     manipulate frame
-# window                              +-----------------------------------+
-# +----------------------------+      |  查詢工具  |   　  mode select     |
-# |      manipulate frame      |      +-----------------------------------+
-# +----------------------------+      | 查詢關鍵字 |     keyword frame     |
-# |         data frame         |      +-----------------------------------+
-# +----------------------------+      |　　　         查詢                 |
-#                                     +-----------------------------------+
+    ButtonMode.set_read_method(mydb.read_data)
+    SQLMode.set_read_method(mydb.exe_query)
 
-Label(manipulate_frame, text='查詢工具'  , bg=BOXCOLOR).grid(row=0, column=0, **PAD)
-Label(manipulate_frame, text='查詢關鍵字', bg=BOXCOLOR).grid(row=1, column=0, **PAD)
-keyword_frame = Frame(manipulate_frame, bg=LINECOLOR)
-keyword_frame.grid(row=1, column=1, **PAD)
-mode_select = ModeSelect(manipulate_frame, values = ['Button', 'MySQL'])
-mode_select.grid(row=0, column=1, **PAD)
-Button(manipulate_frame, text='查詢', command=update, bg='limegreen').grid(row=2, column=0, columnspan=2, **PAD)
+    window = Tk()
+    window.title("Arlen's money tree")
 
-data =Table(data_frame, showtoolbar=True, showstatusbar=True, width=300)
+    manipulate_frame = Frame(window, bg=LINECOLOR) 
+    data_frame = Frame(window, bg= LINECOLOR)
+    manipulate_frame.pack(padx=1, pady=1)
+    data_frame.pack(padx=1, pady=1)
+    #                                     manipulate frame
+    # window                              +-----------------------------------+
+    # +----------------------------+      |  查詢工具  |   　  mode select     |
+    # |      manipulate frame      |      +-----------------------------------+
+    # +----------------------------+      | 查詢關鍵字 |     keyword frame     |
+    # |         data frame         |      +-----------------------------------+
+    # +----------------------------+      |　　　         查詢                 |
+    #                                     +-----------------------------------+
 
-i = 5
-data.model.df = df.head(i)
-data.show()
+    Label(manipulate_frame, text='查詢工具'  , bg=BOXCOLOR).grid(row=0, column=0, **PAD)
+    Label(manipulate_frame, text='查詢關鍵字', bg=BOXCOLOR).grid(row=1, column=0, **PAD)
+    keyword_frame = Frame(manipulate_frame, bg=LINECOLOR)
+    keyword_frame.grid(row=1, column=1, **PAD)
+    mode_select = ModeSelect(manipulate_frame, option = [ButtonMode, SQLMode])
+    mode_select.grid(row=0, column=1, **PAD)
+    datatable =Table(data_frame, showtoolbar=True, showstatusbar=True, width=300)
+    datatable.model.df = DataFrame(data={'location':['高雄'], 'politics':['發大財']})
+    datatable.show()
+    Button(manipulate_frame, text='查詢',
+            command = mode_select.link(datatable), bg='limegreen').grid(row=2, column=0, columnspan=2, **PAD)
 
-window.mainloop()
+    window.mainloop()
+
+    mydb.db.close()
